95228a86ca7cd3808c8601940cdbc65809e84333
Hadeel Kibreet <hadoosh2000k@gmail.com>
2024-07-19 00:34:40 +0300

    translate active_orders and card and add QR Scan

--- lib/constants/images.dart
+++ lib/constants/images.dart
@@ -2,4 +2,5 @@ class ImageAssets {
   static const logo = 'assets/images/logo.jpg';
   static const usa = 'assets/images/Flag_usa.png';
   static const ksa = 'assets/images/flag_ksa.png';
+  static const profile = 'assets/images/profile.png';
 }
