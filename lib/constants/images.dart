a2115f6a68add4939f0483f3dc74eedf69a87ed2
FerasTawuniya <Firas.alshawa@gmail.com>
2024-10-26 17:51:07 +0300

    update assets

--- lib/constants/images.dart
+++ lib/constants/images.dart
@@ -1,5 +1,6 @@
 class ImageAssets {
-  static const logo = 'assets/images/logo.svg';
+  static const logo = 'assets/images/logo.png';
+  static const logoSvg = 'assets/images/logo.svg';
   static const usa = 'assets/images/Flag_usa.png';
   static const ksa = 'assets/images/flag_ksa.png';
   static const profile = 'assets/images/profile.png';
